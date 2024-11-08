4cd2ba16baa587b7b1ba9a416a283df4f1eee611
617998 <sinchuri.617998@swl.k12.oh.us>
2024-11-08 09:44:59 -0500

    2ini

--- src/InputHelper.java
+++ src/InputHelper.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class InputHelper {
+    public static String getRegExString(Scanner scan, String prompt, String regExPattern) {
+        String input;
+        boolean check = false;
+
+        System.out.println(prompt);
+        do {
+            input = scan.nextLine();
+            if (input.matches(regExPattern)) {
+                check = true;
+            } else {
+                System.out.println("Invalid input, please try again.");
+            }
+        } while (!check);
+
+        return input;
+    }
+
 }
--- src/RegExMagic.java
+++ src/RegExMagic.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class RegExMagic {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        //SSID
+        String SSN = InputHelper.getRegExString(scan, "Please enter your SSN with dashes:", "\\d{3}-\\d{2}-\\d{4}");
+        System.out.println("Your SSN is: " + SSN);
+
+        //student UC code
+        String studentMNum = InputHelper.getRegExString(scan, "Please enter your student M number:", "(M|m)\\d{5}");
+        System.out.println("Your student M number: " + studentMNum.toUpperCase());
+
+        //lets you pick to navigate from menu
+        String menuChoice = InputHelper.getRegExString(scan, "Menu: O - Open, S - Save, V - View, Q - Quit", "[OoSsVvQq]");
+        System.out.println("Your choice: " + menuChoice.toUpperCase());
+    }
 }
